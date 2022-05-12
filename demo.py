02868e4fccb6ce2185240325722fae8046e1de3d
TimoRozendal <timo.rozendal@gmail.com>
2022-05-12 09:40:49 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -103,6 +103,31 @@ def make_animation2(morph_video, driving_video, generator, kp_detector,n, relati
             if i is n: break
     return predictions
 
+def make_animation3(morph_video, driving_video, generator, kp_detector,n, relative=True, adapt_movement_scale=True, cpu=False):
+    with torch.no_grad():
+        predictions = []
+        driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
+        kp_driving_initial = kp_detector(driving[:, :, 0])
+        i=0
+        for frame_idx in tqdm(range(driving.shape[2])):
+            source_image=resize(np.asarray(morph_video[i]), (256, 256))
+            source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
+            if not cpu:
+                source = source.cuda()
+            kp_source = kp_detector(source)
+            driving_frame = driving[:, :, frame_idx]
+            if not cpu:
+                driving_frame = driving_frame.cuda()
+            kp_driving = kp_detector(driving_frame)
+            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
+                                   kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
+                                   use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
+            out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
+
+            predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
+            i+=1
+            if i is n: break
+    return predictions
 
 def find_best_frame(source, driving, cpu=False):
     import face_alignment
